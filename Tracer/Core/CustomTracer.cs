a8511116dd5ee1715c43a7891fc17913ead62fec
Maksim Beliavsky <maksimbell503@gmail.com>
2022-09-08 09:34:54 +0300

    add CustomTracer class

---
+++ Tracer/Core/CustomTracer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Tracer
+{
+    public class CustomTracer : ITracer
+    {
+        private Stopwatch stopwatch;
+
+        public TraceResult GetTraceResult()
+        {
+            TraceResult result = new TraceResult(stopwatch.ElapsedMilliseconds);
+
+            return result;
+        }
+
+        public void StartTrace()
+        {
+            var method = new StackTrace().GetFrame(0).GetMethod();
+            stopwatch = new Stopwatch();
+        }
+
+        public void StopTrace()
+        {
+            stopwatch.Stop();
+
+        }
+    }
+}
