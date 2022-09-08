1dc3a2aadccc982b760db4c3bf26528be52bdf7a
Maksim Beliavsky <maksimbell503@gmail.com>
2022-09-08 09:33:40 +0300

    add TraceResult class

---
+++ Tracer/Core/TraceResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class TraceResult
+    {
+        public long Elapsed;
+
+        public TraceResult(long elapsed)
+        {
+            Elapsed = elapsed;
+        }
+    }
+}
