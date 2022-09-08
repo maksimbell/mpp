7e0506ad0cf811faa0fa3415f9e23423efa5706c
Maksim Beliavsky <maksimbell503@gmail.com>
2022-09-08 09:42:58 +0300

    add example classes

---
+++ Tracer/Example/Bar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class Bar
+    {
+        private ITracer _tracer;
+
+        internal Bar(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void InnerMethod()
+        {
+            _tracer.StartTrace();
+            _tracer.StopTrace();
+        }
+    }
+}
