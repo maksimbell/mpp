4fd85c4d7fc260df9854cb46b4633082f9b6f158
Maksim Beliavsky <maksimbell503@gmail.com>
2022-10-03 17:28:13 +0300

    init faker

---
+++ Faker/Faker.Core/Faker.cs
@@ -0,0 +1,15 @@
+﻿namespace Faker.Core
+{
+    public class Faker
+    {
+        public T Create<T>()
+        {
+            return (T)Create(typeof(T));
+        }
+
+        public object Create(Type t)
+        {
+            return new object();
+        }
+    }
+}
