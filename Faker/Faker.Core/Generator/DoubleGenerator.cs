9d4717f73fac1c9567501d55e7441e669f50f96f
Maksim Beliavsky <maksimbell503@gmail.com>
2022-10-06 13:42:24 +0300

    add double generator

---
+++ Faker/Faker.Core/Generator/DoubleGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core.Generator
+{
+    public class DoubleGenerator : IValueGenerator
+    {
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.NextDouble() * (double.MaxValue - double.MinValue) + double.MinValue;
+        }
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(double);
+        }
+    }
+}
