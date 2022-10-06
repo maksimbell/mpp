7497071e0a888e7e52a30188ec626387660a8df1
Maksim Beliavsky <maksimbell503@gmail.com>
2022-10-06 13:36:22 +0300

    add int generator class

---
+++ Faker/Faker.Core/Generator/IntGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core.Generator
+{
+    public class IntGenerator : IValueGenerator
+    {
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.Next(int.MinValue, int.MaxValue);
+        }
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(int);
+        }
+    }
+}
