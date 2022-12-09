889fd6c92f733e4cbdfcc0e16147b751d3445915
Maksim Beliavsky <maksimbell503@gmail.com>
2022-12-09 18:55:56 +0300

    create mstest test project

--- TestsGenerator/TestsGenerator.Tests/UnitTest1.cs
+++ TestsGenerator/TestsGenerator.Tests/UnitTest1.cs
@@ -1,16 +1,11 @@
 namespace TestsGenerator.Tests
 {
-    public class Tests
+    [TestClass]
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
+        [TestMethod]
+        public void TestMethod1()
         {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
         }
     }
 }
