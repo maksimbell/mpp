d7e38352554577e49879763ca4b0357dc863439c
Maksim Beliavsky <maksimbell503@gmail.com>
2022-12-19 22:11:46 +0300

    init 5lab project

---
+++ StringFormatter/StringFormatter/IStringFormatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormatter
+{
+    public interface IStringFormatter
+    {
+        string Format(string template, object target);
+    }
+}
---
+++ StringFormatter/StringFormatter/StringFormatter.cs
@@ -0,0 +1,11 @@
+﻿namespace StringFormatter
+{
+    public class StringFormatter : IStringFormatter
+    {   
+        public static readonly StringFormatter Shared = new StringFormatter();
+        public string Format(string template, object target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
